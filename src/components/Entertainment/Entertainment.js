edd4d9d6d20d9383b49f4d579ef7dd7d3b8ebd2b
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2022-06-03 09:45:25 +0100

    updated the NavBar

---
+++ src/components/Entertainment/Entertainment.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Entertainment.css";
+
+export default function Entertainment() {
+  return <div>Entertainent</div>;
+}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,30 +1,67 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 import "./NavBar.css";
+// import Pix from '../../assets/images/img8.jpg'
 
-export default function NavBar() {
+const NavBar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
-    <div>
-      <NavLink>
-        <ul>
-          <li>
-            <News />
-          </li>
-          <li>
-            <Politics />
-          </li>
-          <li>
-            <Science />
-          </li>
-          <li>
-            <Sport />
-          </li>
-          <li>
-            <Entertainment />
-          </li>
-        </ul>
-      </NavLink>
+    <div className="navbar">
+      <div className="logo">
+        <h1>Kmath</h1>
+      </div>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            News
+          </Link>{" "}
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/politics"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Politics
+          </Link>{" "}
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/science"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Science
+          </Link>{" "}
+        </li>
+
+        <li className="nav-item">
+          <Link to="/sport" style={{ textDecoration: "none", color: "white" }}>
+            Sport
+          </Link>{" "}
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/entertainment"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Entertainment
+          </Link>{" "}
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={30} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={30} style={{ color: "#fff" }} />
+        )}
+      </div>
     </div>
   );
-}
+};
+export default NavBar;
