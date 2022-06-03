409a5eac95eaeaa978fe2835cce94f27f5225d80
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2022-06-03 09:45:25 +0100

    updated the NavBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import News from "./components/News/News";
 import Sport from "./components/Sport/Sport";
+import Science from "./components/Science/Science";
 import NavBar from "./components/NavBar/NavBar";
+import Politics from "./components/Politics/Politics";
+import ErrorPage from "./components/ErrorPage";
+import Entertainment from "./components/Entertainment/Entertainment";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavBar />
-    </div>
+      <Routes>
+        <Route path="/" element={<News />} />
+        <Route path="/sport" element={<Sport />} />
+        <Route path="/science" element={<Science />} />
+        <Route path="/politics" element={<Politics />} />
+        <Route path="/entertainment" element={<Entertainment />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Entertainment/Entertainent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./Entertainent.css";
-
-export default function Entertainent() {
-  return <div>Entertainent</div>;
-}
